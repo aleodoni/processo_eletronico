47f84484c6fb7d830a8d6370697190f2e747dca8
aleodoni <aleodoni@gmail.com>
2019-08-16 16:58:56 -0300

    chore(backend/frontend): commit com o esqueleto do frontend e backend
    Estrutura inicial dos dois projetos, e pacotes básicos.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import GlobalStyle from "./styles/global";
+
+function App() {
+  return (
+    <>
+      <div>Processo Eletrônico</div>
+
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
