41c8ff3f235defa9c34610cc5a951ad5ec208c7e
rodrigoferraro <ferraro.rodrigo@gmail.com>
2020-04-23 16:08:40 -0300

    Create 20200423184607-cria-tabela-genero.js
    migration para tabela [Genero]

---
+++ backend/src/database/migrations/20200423184607-cria-tabela-genero.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('genero', {
+            gen_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true
+            },
+            gen_nome: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            versao: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                defaultValue: 0
+            }
+        },
+        {
+            schema: 'spa2'
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('genero');
+    }
+};
