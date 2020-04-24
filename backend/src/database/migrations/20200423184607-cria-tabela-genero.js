882246082833f31d669f0bc7e344491d18d6b71d
rodrigoferraro <ferraro.rodrigo@gmail.com>
2020-04-24 13:20:00 -0300

    Update 20200423184607-cria-tabela-genero.js
    conforme @aleodoni observou em https://github.com/CMCuritiba/processo_eletronico/pull/60#discussion_r414232466

--- backend/src/database/migrations/20200423184607-cria-tabela-genero.js
+++ backend/src/database/migrations/20200423184607-cria-tabela-genero.js
@@ -10,7 +10,7 @@ module.exports = {
                 primaryKey: true
             },
             gen_nome: {
-                type: Sequelize.STRING,
+                type: Sequelize.STRING(100),
                 allowNull: false
             },
             versao: {
