e8a2adafb333d7da91e1863f31d9358fcf8c45a3
aleodoni <aleodoni@gmail.com>
2020-06-24 18:29:24 -0300

    fix(backend): Atualiza sequência de chave primária na tabela gênero

--- backend/src/database/seeds/20200423190113-genero.js
+++ backend/src/database/seeds/20200423190113-genero.js
@@ -1,37 +1,42 @@
 'use strict';
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.bulkInsert(
-        { tableName: 'genero', schema: 'spa2' },
-        [
-            {
-                gen_id: 4,
-                gen_nome: 'Requerimentos RH',
-                versao: 0
-            },
-            {
-                gen_id: 6,
-                gen_nome: 'Baixa de bens',
-                versao: 0
-            },
-            {
-                gen_id: 7,
-                gen_nome: 'Execução de despesas',
-                versao: 0
-            },
-            {
-                gen_id: 29,
-                gen_nome: 'Presidência',
-                versao: 0
-            },
-            {
-                gen_id: 60,
-                gen_nome: 'Pronto pagamento',
-                versao: 0
-            }
-        ],
-        {}
-    ),
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert(
+            { tableName: 'genero', schema: 'spa2' },
+            [
+                {
+                    gen_id: 4,
+                    gen_nome: 'Requerimentos RH',
+                    versao: 0
+                },
+                {
+                    gen_id: 6,
+                    gen_nome: 'Baixa de bens',
+                    versao: 0
+                },
+                {
+                    gen_id: 7,
+                    gen_nome: 'Execução de despesas',
+                    versao: 0
+                },
+                {
+                    gen_id: 29,
+                    gen_nome: 'Presidência',
+                    versao: 0
+                },
+                {
+                    gen_id: 60,
+                    gen_nome: 'Pronto pagamento',
+                    versao: 0
+                }
+            ],
+            {}
+
+        );
+
+        await queryInterface.sequelize.query('select setval(\'spa2.genero_gen_id_seq\', coalesce(max(gen_id), 0)+1, false) from spa2.genero;');
+    },
 
     down: (queryInterface, Sequelize) => queryInterface.bulkDelete(
         { tableName: 'genero', schema: 'spa2' },
