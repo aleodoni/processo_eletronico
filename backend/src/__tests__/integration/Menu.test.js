ab4dc8073d587a30f7a29952ea390019f8fe38b0
aleodoni <aleodoni@gmail.com>
2020-07-16 16:18:23 -0300

    test(backend): Refatora testes de integração para menu e adiciona novos testes

--- backend/src/__tests__/integration/Menu.test.js
+++ backend/src/__tests__/integration/Menu.test.js
@@ -9,6 +9,8 @@ process.env.NODE_ENV = 'test';
 let token;
 // eslint-disable-next-line no-unused-vars
 let usuario = '';
+// eslint-disable-next-line no-unused-vars
+let menuCreated = '';
 
 beforeAll(done => {
     request(app)
@@ -26,111 +28,180 @@ beforeAll(done => {
 });
 
 describe('Menu', () => {
-    it('dummy', () => {
-        expect(true).toBe(true);
+    it('Deve retornar lista de menus', async() => {
+        const response = await request(app)
+            .get(`${process.env.API_URL}/menu`)
+            .set('authorization', `${token}`);
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    men_id: 47,
+                    men_nome: 'Complementar dados'
+                })
+            ])
+        );
     });
-    // it('Menu - Lista', function(done) {
-    //     request(app)
-    //         .get(`${process.env.API_URL}/menu`)
-    //         .set('authorization', `${token}`)
-    //         .expect(200)
-    //         .end(function(err) {
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // it('Telas do Menu', function(done) {
-    //     request(app)
-    //         .get(`${process.env.API_URL}/tela-menu`)
-    //         .set('authorization', `${token}`)
-    //         .expect(200)
-    //         .end(function(err) {
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // it('Menu pai', function(done) {
-    //     request(app)
-    //         .get(`${process.env.API_URL}/menu-pai`)
-    //         .set('authorization', `${token}`)
-    //         .expect(200)
-    //         .end(function(err) {
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // let menuId = '';
-    // it('Menu - Insere', function(done) {
-    //     const insereMenus = {
-    //         men_id: null,
-    //         men_id_pai: null,
-    //         men_nome: `Inserção nome menu - ${Math.random()}`,
-    //         men_url: `Inserção url menu - ${Math.random()}`,
-    //         tel_id: null,
-    //         tel_interna: true
-    //     };
-    //     request(app)
-    //         .post(`${process.env.API_URL}/menu`)
-    //         .set('authorization', `${token}`)
-    //         .set('usuario', `${usuario}`)
-    //         .send(insereMenus)
-    //         .set('Content-Type', 'application/json')
-    //         .expect(200)
-    //         .end(function(err, res) {
-    //             menuId = res.body.men_id;
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // it('Menu - Edita', function(done) {
-    //     const editaMenus = {
-    //         men_id_pai: null,
-    //         men_nome: `Edição nome menu - ${Math.random()}`,
-    //         men_url: `Edição url menu - ${Math.random()}`,
-    //         tel_id: null,
-    //         tel_interna: true
-    //     };
-    //     request(app)
-    //         .put(`${process.env.API_URL}/menu/${menuId}`)
-    //         .set('authorization', `${token}`)
-    //         .set('usuario', `${usuario}`)
-    //         .send(editaMenus)
-    //         .set('Content-Type', 'application/json')
-    //         .expect(200)
-    //         .end(function(err, _res) {
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // it('Menu - Apaga', function(done) {
-    //     request(app)
-    //         .delete(`${process.env.API_URL}/menu/${menuId}`)
-    //         .set('authorization', `${token}`)
-    //         .set('usuario', `${usuario}`)
-    //         .set('Content-Type', 'application/json')
-    //         .expect(200)
-    //         .end(function(err, res) {
-    //             menuId = res.body.men_id;
-    //             if (err) {
-    //                 return done(err);
-    //             }
-    //             return done();
-    //         });
-    // });
-    // test('Geração do menu', async() => {
-    //     const response = await request(app)
-    //         .get(`${process.env.API_URL}/geraMenu/027`)
-    //         .set('authorization', `${token}`);
-    //     expect(response.statusCode).toBe(200);
-    // });
+
+    it('Deve retornar lista de telas do menu', async() => {
+        const response = await request(app)
+            .get(`${process.env.API_URL}/tela-menu`)
+            .set('authorization', `${token}`);
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    men_id: 47,
+                    men_nome: 'Complementar dados'
+                })
+            ])
+        );
+    });
+
+    it('Deve retornar lista de menu pai', async() => {
+        const response = await request(app)
+            .get(`${process.env.API_URL}/menu-pai`)
+            .set('authorization', `${token}`)
+            .expect(200);
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    men_id: 440,
+                    nome_pai: 'Dados processo - Modelo informática'
+                })
+            ])
+        );
+    });
+
+    it('Deve inserir um menu com sucesso', async() => {
+        const insereMenu = {
+            men_id: null,
+            men_id_pai: null,
+            men_nome: `Inserção nome menu - ${Math.random()}`,
+            men_url: `Inserção url menu - ${Math.random()}`,
+            tel_id: 22,
+            mmu_id: 1,
+            tel_interna: true
+        };
+
+        const response = await request(app)
+            .post(`${process.env.API_URL}/menu`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`)
+            .send(insereMenu);
+
+        menuCreated = response.body;
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toHaveProperty('men_nome', insereMenu.men_nome);
+    });
+
+    it('Deve alterar um menu com sucesso', async() => {
+        const alteraMenu = {
+            men_id_pai: null,
+            men_nome: 'Menu Alterado',
+            men_url: menuCreated.men_url,
+            tel_id: 22,
+            mmu_id: 1,
+            tel_interna: true
+        };
+
+        const response = await request(app)
+            .put(`${process.env.API_URL}/menu/${menuCreated.men_id}`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`)
+            .send(alteraMenu);
+
+        menuCreated = response.body;
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toHaveProperty('men_nome', 'Menu Alterado');
+    });
+
+    it('Deve deletar um menu', async() => {
+        const response = await request(app)
+            .delete(`${process.env.API_URL}/menu/${menuCreated.men_id}`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`);
+
+        expect(response.status).toBe(200);
+    });
+
+    it('Não deve inserir um  menu com o nome nulo', async() => {
+        const insereMenu = {
+            men_id: null,
+            men_id_pai: null,
+            men_nome: null,
+            men_url: `Inserção url menu - ${Math.random()}`,
+            tel_id: 22,
+            mmu_id: 1,
+            tel_interna: true
+        };
+
+        const response = await request(app)
+            .post(`${process.env.API_URL}/menu`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`)
+            .send(insereMenu);
+
+        expect(response.status).toBe(422);
+
+        const errorParsed = JSON.parse(response.text);
+        expect(errorParsed.message).toBe('Nome menu obrigatório');
+    });
+
+    it('Não deve inserir um menu com o nome maior que 60 caracteres', async() => {
+        const insereMenu = {
+            men_id: null,
+            men_id_pai: null,
+            men_nome: '1'.repeat(61),
+            men_url: `Inserção url menu - ${Math.random()}`,
+            tel_id: 22,
+            mmu_id: 1,
+            tel_interna: true
+        };
+
+        const response = await request(app)
+            .post(`${process.env.API_URL}/menu`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`)
+            .send(insereMenu);
+
+        expect(response.status).toBe(422);
+
+        const errorParsed = JSON.parse(response.text);
+        expect(errorParsed.message).toBe('Nome menu não pode ter mais que 60 caracteres');
+    });
+
+    it('Não deve inserir um menu com o url maior que 200 caracteres', async() => {
+        const insereMenu = {
+            men_id: null,
+            men_id_pai: null,
+            men_nome: 'Teste nome',
+            men_url: '1'.repeat(201),
+            tel_id: 22,
+            mmu_id: 1,
+            tel_interna: true
+        };
+
+        const response = await request(app)
+            .post(`${process.env.API_URL}/menu`)
+            .set('authorization', `${token}`)
+            .set('usuario', `${usuario}`)
+            .send(insereMenu);
+
+        expect(response.status).toBe(422);
+
+        const errorParsed = JSON.parse(response.text);
+        expect(errorParsed.message).toBe('Url menu não pode ter mais que 200 caracteres');
+    });
 });
