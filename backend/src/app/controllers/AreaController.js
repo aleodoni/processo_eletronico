dead6668aff0a019b427b759247fa0d8caae67ed
aleodoni <aleodoni@gmail.com>
2020-08-06 18:54:05 -0300

    feat(backend): Refatora AreaController para utilizar serviços

--- backend/src/app/controllers/AreaController.js
+++ backend/src/app/controllers/AreaController.js
@@ -1,59 +1,72 @@
 import Setor from '../models/Setor';
 import AreaCombo from '../models/AreaCombo';
-import Sequelize from 'sequelize';
+
+// import ConnectionHelper from '../helpers/ConnectionHelper';
+// import Sequelize from 'sequelize';
+import ListAllAreaService from '../services/area/ListAllAreaService';
+import ListAllAreaNormalService from '../services/area/ListAllAreaNormalService';
+import FindAreaPorCodigoService from '../services/area/FindAreaPorCodigoService';
+import FindSetorPorCodigoService from '../services/area/FindSetorPorCodigoService';
 
 class AreaController {
     async index(req, res) {
-        const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-            host: process.env.DB_HOST,
-            dialect: 'postgres',
-            define: {
-                timestamps: false,
-                underscoredAll: true
-            },
-            pool: {
-                max: 7,
-                min: 0,
-                acquire: 30000,
-                idle: 10000
-            }
-        });
-        const areas = await Setor.findAll({
-            where: sequelize.where(
-                sequelize.col('set_id'),
-                sequelize.col('set_id_area')
-            ),
+        const listAllArea = new ListAllAreaService(Setor);
 
-            order: ['set_nome'],
-            attributes: ['set_id', 'set_nome'],
-            logging: false
-        });
+        const areas = await listAllArea.execute();
+        // const connection = ConnectionHelper.getConnection();
+
+        // const areas = await Setor.findAll({
+        //     where: Sequelize.where(
+        //         Sequelize.col('set_id'),
+        //         Sequelize.col('set_id_area')
+        //     ),
+
+        //     order: ['set_nome'],
+        //     attributes: ['set_id', 'set_nome'],
+        //     logging: false
+        // });
         return res.json(areas);
     }
 
     async areaNormal(req, res) {
-        const areas = await AreaCombo.findAll({
-            where: { set_tipo: 'N' },
-            order: ['set_nome'],
-            attributes: ['set_id', 'set_nome'],
-            logging: false
-        });
+        const listAllAreaNormal = new ListAllAreaNormalService(AreaCombo);
+
+        const areas = await listAllAreaNormal.execute();
+        // const areas = await AreaCombo.findAll({
+        //     where: { set_tipo: 'N' },
+        //     order: ['set_nome'],
+        //     attributes: ['set_id', 'set_nome'],
+        //     logging: false
+        // });
         return res.json(areas);
     }
 
     async areaPorCodigo(req, res) {
-        const area = await Setor.findByPk(req.params.id, { logging: false });
-        if (!area) {
-            return res.status(400).json({ error: 'Área não encontrada' });
-        }
+        const findAreaPorCodigo = new FindAreaPorCodigoService(Setor);
+
+        const { id } = req.params;
+
+        const area = await findAreaPorCodigo.execute({ id });
+
+        // const area = await Setor.findByPk(req.params.id, { logging: false });
+        // if (!area) {
+        //     return res.status(400).json({ error: 'Área não encontrada' });
+        // }
         return res.json(area);
     }
 
     async setorPorCodigo(req, res) {
-        const setor = await Setor.findByPk(req.params.id, { logging: false });
-        if (!setor) {
-            return res.status(400).json({ error: 'Setor não encontrado' });
-        }
+        const findSetorPorCodigo = new FindSetorPorCodigoService(Setor);
+
+        const { id } = req.params;
+
+        const setor = await findSetorPorCodigo.execute({ id });
+
+        // const setor = await Setor.findByPk(req.params.id, { logging: false });
+        // if (!setor) {
+        //     return res.status(400).json({ error: 'Setor não encontrado' });
+        // }
+
         return res.json(setor);
     }
 }
