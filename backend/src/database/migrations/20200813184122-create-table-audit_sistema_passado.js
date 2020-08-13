007fba7d77a0092d46da94c193faa13668b70cca
aleodoni <aleodoni@gmail.com>
2020-08-13 15:44:00 -0300

    feat(backend): Cria migration para tabela audit_sistema_passado

---
+++ backend/src/database/migrations/20200813184122-create-table-audit_sistema_passado.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('audit_sistema_passado', {
+            rea_tela: {
+                type: Sequelize.STRING
+            },
+            rea_login: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            rea_terminal: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            rea_data: {
+                type: Sequelize.DATE,
+                allowNull: false
+            },
+            rea_operacao: {
+                type: Sequelize.STRING(1),
+                allowNull: false
+            },
+            rea_campo: {
+                type: Sequelize.STRING
+            },
+            rea_valor_anterior: {
+                type: Sequelize.STRING
+            },
+            rea_chave: {
+                type: Sequelize.STRING,
+                allowNull: false
+            }
+        },
+        {
+            schema: 'spa2'
+        });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        return queryInterface.dropTable('audit_sistem_passado');
+    }
+};
