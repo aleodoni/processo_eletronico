0ec174a3adbba236f4ce0ab25f1ea71cdc6bf4c7
aleodoni <aleodoni@gmail.com>
2020-10-23 08:20:49 -0300

    feat(backend): Adiciona campo na tabela autorizacao_fornecimento

---
+++ backend/src/database/migrations/20201019184452-add-aut_fatura_boleto-table-autorizacao_fornecimento.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const sequelize = require('sequelize');
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await queryInterface.addColumn({
+                tableName: 'autorizacao_fornecimento',
+                schema: 'spa2'
+            },
+            'aut_fatura_boleto', {
+                type: sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            }, { transaction });
+
+            await transaction.commit();
+
+            return Promise.resolve();
+        } catch (err) {
+            await transaction.rollback();
+            return Promise.reject(err);
+        }
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await transaction.commit();
+
+            return Promise.resolve();
+        } catch (err) {
+            await transaction.rollback();
+            return Promise.reject(err);
+        }
+    }
+};
