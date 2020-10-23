1656affb8d242f644d87797f91a00670f5291d56
aleodoni <aleodoni@gmail.com>
2020-10-23 08:18:30 -0300

    feat(backend): Adiciona campo na tabela arquivo

---
+++ backend/src/database/migrations/20201019183823-add-arq_cancelado-table-arquivo.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const sequelize = require('sequelize');
+
+module.exports = {
+    up: async(queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await queryInterface.addColumn({
+                tableName: 'arquivo',
+                schema: 'spa2'
+            },
+            'arq_cancelado', {
+                type: sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            }, { transaction });
+
+            await transaction.commit();
+
+            return Promise.resolve();
+        } catch (err) {
+            await transaction.rollback();
+            return Promise.reject(err);
+        }
+    },
+
+    down: async(queryInterface, Sequelize) => {
+        const transaction = await queryInterface.sequelize.transaction();
+        try {
+            await transaction.commit();
+
+            return Promise.resolve();
+        } catch (err) {
+            await transaction.rollback();
+            return Promise.reject(err);
+        }
+    }
+};
