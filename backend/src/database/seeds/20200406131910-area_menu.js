3ab5c2718ef5f9fc24d6c6f8a4818cc1a594cb15
aleodoni <aleodoni@gmail.com>
2020-11-13 08:12:58 -0300

    feat(backend): Atualiza seed com dados necessários para funcionamento do sistema.

--- backend/src/database/seeds/20200406131910-area_menu.js
+++ backend/src/database/seeds/20200406131910-area_menu.js
@@ -5,156 +5,34 @@ module.exports = {
         await queryInterface.bulkInsert(
             { tableName: 'area_menu', schema: 'spa2' },
             [
-                {
-                    amu_id: 1,
-                    set_id: '027',
-                    mmu_id: 1,
-                    versao: 0
-                },
-                {
-                    amu_id: 2,
-                    set_id: '032',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 109,
-                    set_id: '288',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 139,
-                    set_id: '287',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 3,
-                    set_id: '168',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 378,
-                    set_id: '389',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 379,
-                    set_id: '001',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 380,
-                    set_id: '004',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 381,
-                    set_id: '007',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 382,
-                    set_id: '365',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 383,
-                    set_id: '048',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 384,
-                    set_id: '149',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 385,
-                    set_id: '448',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 386,
-                    set_id: '088',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 387,
-                    set_id: '143',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 388,
-                    set_id: '104',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 389,
-                    set_id: '289',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 390,
-                    set_id: '299',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 391,
-                    set_id: '131',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 392,
-                    set_id: '141',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 393,
-                    set_id: '122',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 394,
-                    set_id: '319',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 395,
-                    set_id: '149',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 396,
-                    set_id: '382',
-                    mmu_id: 2,
-                    versao: 0
-                },
-                {
-                    amu_id: 397,
-                    set_id: '383',
-                    mmu_id: 2,
-                    versao: 0
-                }
+                { amu_id: 1, set_id: '027', versao: 0, mmu_id: 1 },
+                { amu_id: 2, set_id: '032', versao: 0, mmu_id: 2 },
+                { amu_id: 109, set_id: '288', versao: 0, mmu_id: 2 },
+                { amu_id: 139, set_id: '287', versao: 0, mmu_id: 2 },
+                { amu_id: 378, set_id: '398', versao: 0, mmu_id: 2 },
+                { amu_id: 379, set_id: '001', versao: 0, mmu_id: 2 },
+                { amu_id: 381, set_id: '007', versao: 0, mmu_id: 2 },
+                { amu_id: 382, set_id: '365', versao: 0, mmu_id: 2 },
+                { amu_id: 383, set_id: '048', versao: 0, mmu_id: 2 },
+                { amu_id: 384, set_id: '149', versao: 0, mmu_id: 2 },
+                { amu_id: 385, set_id: '448', versao: 0, mmu_id: 2 },
+                { amu_id: 386, set_id: '088', versao: 0, mmu_id: 2 },
+                { amu_id: 388, set_id: '104', versao: 0, mmu_id: 2 },
+                { amu_id: 389, set_id: '289', versao: 0, mmu_id: 2 },
+                { amu_id: 390, set_id: '299', versao: 0, mmu_id: 2 },
+                { amu_id: 392, set_id: '141', versao: 0, mmu_id: 2 },
+                { amu_id: 394, set_id: '319', versao: 0, mmu_id: 2 },
+                { amu_id: 395, set_id: '149', versao: 0, mmu_id: 2 },
+                { amu_id: 396, set_id: '382', versao: 0, mmu_id: 2 },
+                { amu_id: 397, set_id: '383', versao: 0, mmu_id: 2 },
+                { amu_id: 404, set_id: '1500', versao: 0, mmu_id: 423 },
+                { amu_id: 398, set_id: '098', versao: 0, mmu_id: 2 },
+                { amu_id: 399, set_id: '100', versao: 0, mmu_id: 2 },
+                { amu_id: 380, set_id: '004', versao: 0, mmu_id: 3 },
+                { amu_id: 400, set_id: '168', versao: 0, mmu_id: 3 },
+                { amu_id: 405, set_id: '385', versao: 0, mmu_id: 2 },
+                { amu_id: 407, set_id: '459', versao: 0, mmu_id: 2 },
+                { amu_id: 406, set_id: '065', versao: 0, mmu_id: 2 }
             ],
             {}
         );
