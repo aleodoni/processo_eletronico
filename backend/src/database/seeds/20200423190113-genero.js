6577d220fe70cf621e6232f34735c416545a48f9
aleodoni <aleodoni@gmail.com>
2020-11-13 08:13:18 -0300

    feat(backend): Atualiza seed com dados necessários para funcionamento do sistema.

--- backend/src/database/seeds/20200423190113-genero.js
+++ backend/src/database/seeds/20200423190113-genero.js
@@ -15,25 +15,25 @@ module.exports = {
                     gen_id: 6,
                     gen_nome: 'Baixa de bens',
                     versao: 0,
-                    gen_visivel: true
+                    gen_visivel: false
                 },
                 {
                     gen_id: 7,
                     gen_nome: 'Execução de despesas',
                     versao: 0,
-                    gen_visivel: true
+                    gen_visivel: false
                 },
                 {
                     gen_id: 29,
                     gen_nome: 'Presidência',
                     versao: 0,
-                    gen_visivel: true
+                    gen_visivel: false
                 },
                 {
                     gen_id: 60,
                     gen_nome: 'Pronto pagamento',
                     versao: 0,
-                    gen_visivel: true
+                    gen_visivel: false
                 }
             ],
             {}
